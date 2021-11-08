ed6e85ee25b250ac9f3bf96a6472c6a5c5a3a980
Burkov Egor <wooftown@yandex.ru>
2021-11-08 17:40:11 +0300

    What a Terrible Failure: I forgot set layout for navigation

--- projects/lab3/navigation/src/main/java/com/wooftown/navigation/AboutActivity.kt
+++ projects/lab3/navigation/src/main/java/com/wooftown/navigation/AboutActivity.kt
@@ -1,5 +1,11 @@
 package com.wooftown.navigation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class AboutActivity : AppCompatActivity()
+class AboutActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about_activity)
+    }
+}
